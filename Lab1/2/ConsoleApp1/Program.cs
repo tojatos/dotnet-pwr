7e4af517686ec2c92514249ba30afa3e49959027
tojatos <tojatos@gmail.com>
2020-10-07 14:25:09 +0200

    Lab1 done

---
+++ Lab1/2/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= size; ++i)
+            {
+                int spaces = size - i;
+                Console.Write(new string(' ', spaces));
+                Console.Write(new string('X', i));
+                Console.WriteLine();
+            }
+        }
+    }
+}
